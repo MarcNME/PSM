0b848bf95f0ed3fe2a7b936bb67f9b55ecb63fb4
Marc Enzmann <marc.enzmann@satis-faction.de>
2022-04-29 08:39:27 +0200

    Gehalt Kalkulation implementiert

--- PSM-Library/model/Report.cs
+++ PSM-Library/model/Report.cs
@@ -6,7 +6,7 @@ namespace PSM_Libary.model
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
-        public double Hours { get; set; }
+        public int Hours { get; set; }
         public int EmployeeId { get; set; }
         public int ActivityId { get; set; }
         public int OrderId { get; set; }
